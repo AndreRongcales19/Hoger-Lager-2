766a9e4704715d09b6b8ea410f6eb4ec5c342f6f
AndreRongcales19 <302334829@student.rocmondriaan.nl>
2023-09-18 16:07:57 +0200

    main.js toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("main.js werkt");
+
+
+const btnThrow = document.querySelector('.throw');
+const hiText = document.querySelector('.hi');
+btnThrow.addEventListener("click", function(){})
+
+
+
+//eerts dubbelsteen
+function getRandomInt(max) {
+
+    return Math.ceil(Math.random() * max);
+  }
+  const yourNumber = getRandomInt(6);
+  console.log(yourNumber);
+
+//tweede dubblsteen
+function getRandomInt(max) {
+
+    return Math.ceil(Math.random() * max);
+  }
+  const computerNumber = getRandomInt(6);
+  console.log(computerNumber);
+
