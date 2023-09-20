cb74f7cac83239fc4ac284482841a4f1aea3f82f
AndreRongcales19 <302334829@student.rocmondriaan.nl>
2023-09-20 11:27:32 +0200

    .

---
+++ main.js
@@ -0,0 +1,14 @@
+function getRandomInt(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  const myNumber1 = getRandomInt(6);
+  console.log(myNumber1)
+
+function getRandomInt(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  const myNumber2 = getRandomInt(6);
+  console.log(myNumber2)  
+
+
+
