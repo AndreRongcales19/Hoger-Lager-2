7880b42f08abb7856d8492a1ce8598ad8da31c72
AndreRongcales19 <302334829@student.rocmondriaan.nl>
2023-09-29 10:31:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
 const btnThrow = document.querySelector(".throw");
+const btnHigher = document.querySelector(".higher");
+const btnLower = document.querySelector(".lower");
+const btnStart = document.querySelector(".start");
+
 const firstDice = document.querySelector('.first-dice');
 const secondDice = document.querySelector('.second-dice');
 const thirdDice = document.querySelector('.third-dice');
 const fourthDice = document.querySelector('.fourth-dice');
+
+btnThrow.disabled = true; 
+btnHigher.disabled = true; 
+btnLower.disabled = true; 
+
+btnStart.addEventListener('click', function (){
+  //deerde dubbelsteen
+
+function getRandomInt(max) {
+  return Math.ceil(Math.random() * max);
+  }
+  const myNumber3 = getRandomInt(6);
+  console.log(myNumber3);
+  thirdDice.src = "./img/dice-" + myNumber3 + ".png"; 
+//vierde dubbelsteen
+
+function getRandomInt(max) {
+  return Math.ceil(Math.random() * max);
+  }
+  const myNumber4 = getRandomInt(6);
+  console.log(myNumber4);
+  fourthDice.src = "./img/dice-" + myNumber4 + ".png";  
+
+  btnStart.disabled = true;
+  btnHigher.disabled = false;
+  btnLower.disabled = false;
+})
+
+btnHigher.addEventListener('click', function(){
+  btnHigher.disabled = true;
+  btnLower.disabled = true;
+  btnThrow.disabled = false;
+})
+
+btnLower.addEventListener('click', function(){
+  btnLower.disabled = true;
+  btnHigher.disabled = true;
+  btnThrow.disabled = false; 
+})
+
 btnThrow.addEventListener('click', function(){
   //eerste dubbelsteen//
+
 function getRandomInt(max) {
   return Math.ceil(Math.random() * max);
   }
@@ -22,25 +67,25 @@ function getRandomInt(max) {
   console.log(myNumber2) 
   secondDice.src = "./img/dice-" + myNumber2 + ".png"; 
 
-//deerde dubbelsteen
+   //deerde dubbelsteen
 
 function getRandomInt(max) {
   return Math.ceil(Math.random() * max);
   }
   const myNumber3 = getRandomInt(6);
-  console.log(myNumber3) 
+  console.log(myNumber3);
   thirdDice.src = "./img/dice-" + myNumber3 + ".png"; 
-
 //vierde dubbelsteen
 
 function getRandomInt(max) {
   return Math.ceil(Math.random() * max);
   }
   const myNumber4 = getRandomInt(6);
-  console.log(myNumber4)
-  fourthDice.src = "./img/dice-" + myNumber4 + ".png";  
+  console.log(myNumber4);
+  fourthDice.src = "./img/dice-" + myNumber4 + ".png"; 
 
-
+  
+  
 // const thrownDice1 = document.querySelector('.thrown-dice1');
 // const thrownDice2 = document.querySelector('.thrown-dice2');
 
@@ -59,7 +104,8 @@ if (result1 > result2) {
 if (result1 === result2) {
   announcementBlock.textContent = "Gooi het nog een keer!"
 }
-})
+});
+
 
 
 
