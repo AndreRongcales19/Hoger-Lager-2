fb5df5cd6832a550beaff54ce41a163797625e10
AndreRongcales19 <302334829@student.rocmondriaan.nl>
2023-10-10 15:04:04 +0200

    .

--- main.js
+++ main.js
@@ -1,109 +1,171 @@
+// Html elementen binenenhalen
 const btnThrow = document.querySelector(".throw");
 const btnHigher = document.querySelector(".higher");
 const btnLower = document.querySelector(".lower");
 const btnStart = document.querySelector(".start");
+const yourScore = document.querySelector(".your-score");
+const computerScore = document.querySelector(".computer-score");
 
 const firstDice = document.querySelector('.first-dice');
 const secondDice = document.querySelector('.second-dice');
 const thirdDice = document.querySelector('.third-dice');
 const fourthDice = document.querySelector('.fourth-dice');
 
-btnThrow.disabled = true; 
-btnHigher.disabled = true; 
-btnLower.disabled = true; 
+// Variabelen
+let higher;
+let diceUser1;
+let diceUser2;
+let diceComp1;
+let diceComp2;
 
-btnStart.addEventListener('click', function (){
-  //deerde dubbelsteen
 
-  function getRandomInt(max) {
+// buttons disabled maken
+btnThrow.disabled = true;
+btnHigher.disabled = true;
+btnLower.disabled = true;
+
+// initialisatie variablen
+let yourPoints = 10;
+let computerPoints = 10;
+yourScore.textContent = yourPoints;
+computerScore.textContent = computerPoints;
+
+// random getallen genereren
+function getRandomInt(max) {
   return Math.ceil(Math.random() * max);
-  }
-  const myNumber3 = getRandomInt(6);
-  console.log(myNumber3);
-  thirdDice.src = "./img/dice-" + myNumber3 + ".png"; 
-  //vierde dubbelsteen
+}
 
-  function getRandomInt(max) {
-  return Math.ceil(Math.random() * max);
-  }
-  const myNumber4 = getRandomInt(6);
-  console.log(myNumber4);
-  fourthDice.src = "./img/dice-" + myNumber4 + ".png";  
+btnStart.addEventListener('click', function () {
+  //eerste dubbelsteen van computer
+  diceComp1 = getRandomInt(6);
+  console.log(diceComp1);
+  thirdDice.src = "./img/dice-" + diceComp1 + ".png";
+
+  //tweede dubbelsteen van computer
+  diceComp2 = getRandomInt(6);
+  console.log(diceComp2);
+  fourthDice.src = "./img/dice-" + diceComp2 + ".png";
 
   btnStart.disabled = true;
   btnHigher.disabled = false;
   btnLower.disabled = false;
 
-  btnHigher.addEventListener('click', function(){
-    btnHigher.disabled = true;
-    btnLower.disabled = true;
-    btnThrow.disabled = false;
-  })
-  
-  btnLower.addEventListener('click', function(){
-    btnLower.disabled = true;
-    btnHigher.disabled = true;
-    btnThrow.disabled = false; 
-  })
-  
-  btnThrow.addEventListener('click', function(){
-    //eerste dubbelsteen//
-  
-    function getRandomInt(max) {
-    return Math.ceil(Math.random() * max);
-    }
-    const myNumber1 = getRandomInt(6);
-    console.log(myNumber1)
-    firstDice.src = "./img/dice-" + myNumber1 + ".png";
-  
-  
-    //tweede dubbelsteen
-  
-    function getRandomInt(max) {
-    return Math.ceil(Math.random() * max);
-    }
-    const myNumber2 = getRandomInt(6);
-    console.log(myNumber2) 
-    secondDice.src = "./img/dice-" + myNumber2 + ".png"; 
-    
-    // const thrownDice1 = document.querySelector('.thrown-dice1');
-    // const thrownDice2 = document.querySelector('.thrown-dice2');
-  
-    const result1 = myNumber1 + myNumber2;
-    const result2 = myNumber3 + myNumber4;
-    // thrownDice1.textContent = result1;
-    // thrownDice2.textContent = result2;
-  
-    const announcementBlock = document.querySelector(".announcement")
-    if (result1 > result2) {
-    announcementBlock.textContent = "Je hebt gewonnen!";
-    } else {
-    announcementBlock.textContent = "Je hebt verloren!"
-    }
-  
-    if (result1 === result2) {
-    announcementBlock.textContent = "Gooi het nog een keer!"
-    }
-    });
 })
 
+// AddEventListeners
+btnHigher.addEventListener('click', function () {
+  btnHigher.disabled = true;
+  btnLower.disabled = true;
+  btnThrow.disabled = false;
+
+  higher = true;
+})
+
+btnLower.addEventListener('click', function () {
+  btnLower.disabled = true;
+  btnHigher.disabled = true;
+  btnThrow.disabled = false;
+
+  high = false;
+})
+
+btnThrow.addEventListener('click', function () {
+  //eerste dubbelsteen van user//
+  diceUser1 = getRandomInt(6);
+  console.log(diceUser1)
+  firstDice.src = "./img/dice-" + diceUser1 + ".png";
+
+  //tweede dubbelsteen van user
+  diceUser2 = getRandomInt(6);
+  console.log(diceUser2)
+  secondDice.src = "./img/dice-" + diceUser2 + ".png";
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const diceUserTotal = diceUser1 + diceUser2;
+  const diceCompTotal = diceComp1 + diceComp2;
+  // We gaan kijken of gebruiker hoger of lager heeft geklikt
   
+  if (higher) {
+    // checken voor hoger
+    if(diceUserTotal > diceCompTotal) {
+      // user heeft gewonnen --> user krijgt + punt en computer - punt
+      yourPoints++;
+      computerPoints--;
+
+      yourScore.textContent = yourPoints;
+      computerScore.textContent = computerPoints;
+    } else if(diceUserTotal < diceCompTotal) {
+      // computer heeft gewonnen --> user krijgt - punt en computer + punt
+      yourPoints--;
+      computerPoints++;
+
+      yourScore.textContent = yourPoints;
+      computerScore.textContent = computerPoints;
+    } else {
+      // gelijkspel
+    }
+  } else {
+    // checken voor lager
+    if(diceUserTotal < diceCompTotal) {
+      // user heeft gewonnen --> user krijgt + punt en computer - punt
+      yourPoints++;
+      computerPoints--;
+      
+      yourScore.textContent = yourPoints;
+      computerScore.textContent = computerPoints;
+    } else if(diceUserTotal > diceCompTotal) {
+      // computer heeft gewonnen --> user krijgt - punt en computer + punt
+      yourPoints--;
+      computerPoints++;
+
+      yourScore.textContent = yourPoints;
+      computerScore.textContent = computerPoints;
+    } else {
+      // gelijkspel
+    }
+  }
+  btnThrow.disabled = true;
+    btnStart.disabled = false;
+  
+
+
+
+  // const thrownDice1 = document.querySelector('.thrown-dice1');
+  // const thrownDice2 = document.querySelector('.thrown-dice2');
+
+  // const result1 = diceUser1 + diceUser2;
+  // const result2 = diceComp1 + diceComp2;
+  // thrownDice1.textContent = result1;
+  // thrownDice2.textContent = result2;
+
+  // const announcementBlock = document.querySelector(".announcement")
+  // if (result1 > result2) {
+  //   announcementBlock.textContent = "Je hebt gewonnen!";
+  // } else {
+  //   announcementBlock.textContent = "Je hebt verloren!"
+  // }
+
+  // if (result1 === result2) {
+  //   announcementBlock.textContent = "Gooi het nog een keer!"
+  // }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
